4211ace5b676d69f50dd1e3784e5c45860345885
ElGringoBonito <messages.lc@gmail.com>
2015-01-12 05:49:30 -0500

    Plot 1 Update

--- plot1.R
+++ plot1.R
@@ -1,19 +1,8 @@
-dataset=read.table("household_power_consumption.txt",sep=";")
-
+dataset <- read.table("household_power_consumption.txt",sep=";",header=TRUE,stringsAsFactors=FALSE,na.strings="?")
 names(dataset)=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-
-dataset=dataset[1:(nrow(dataset)/3),]
-
-dataset$Date=as.Date(dataset$Date, "%d/%m/%Y")
-
-dd=dataset[(dataset$Date=="2007-02-01" | dataset$Date=="2007-02-02"),]
-dd$Date=as.Date(dd$Date,"%Y-%m-%d")
-
-dds=dd[(as.Date(dd$Date,"%Y-%m-%d")=="2007-02-02" | as.Date(dd$Date,"%Y-%m-%d")=="2007-02-01"),]
-
-dds$Global_active_power=as.numeric(dds$Global_active_power)
-dds$Global_active_power=dds$Global_active_power/500
+dataset=dataset[(dataset$Date=="1/2/2007" | dataset$Date=="2/2/2007"),]
+dataset$DT=as.POSIXct(strptime(paste(dataset$Date," ",dataset$Time,sep=""),"%d/%m/%Y %H:%M:%S"))
 
 png('plot1.png', height=480, width=480)
-hist(dds$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power",lab=c(4,7))
+hist(dataset$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power",lab=c(4,7))
 dev.off()
